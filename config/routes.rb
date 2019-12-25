09407ae8c6a3b6100e35232964968cf97cbb54a8
hiromu0124 <totohiro0124@icloud.com>
2019-12-25 20:37:16 +0900

    ビューの編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-
   root  'products#index'
   resources :products, only: [:index, :new, :show]
   resources :users, only: [:show]
+  get "RedBull", to: "users#edit2"
 end

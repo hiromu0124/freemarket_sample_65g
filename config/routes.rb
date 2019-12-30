5a8dfe23e1edd40a128e0023586137e3ae97566e
nakashun1129 <nakashun1129@icloud.com>
2019-12-30 13:26:15 +0900

    devise管理下のusersコントローラを作成し設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+  }
   root to: 'products#index'
-  resources :products, only: [:index, :new, :show]
+  resources :products, only: [:index, :new, :show, :create]
   resources :users, only: [:show]
 
   get "RedBull", to:"users#edit2"

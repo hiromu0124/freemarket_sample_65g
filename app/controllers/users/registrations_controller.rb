35e713f9b588dcd523002ed47819d67571a19767
nakashun1129 <nakashun1129@icloud.com>
2020-01-01 16:12:46 +0900

    ウィザード形式に使用するファイル作成

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
-  # def new
-  #   super
-  # end
+  def new
+    @user = User.new
+  end
 
   # POST /resource
   # def create
@@ -38,12 +38,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,16 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations',
   }
+  devise_scope :user do
+    get 'phone-number', to: 'users/registrations#new_phone-number'
+    get 'street-address', to: 'users/registrations#new_street-address'
+    get 'payment', to: 'users/registrations#new_payment'
+    get 'done', to: 'users/registrations#done'
+  end
   root to: 'products#index'
   resources :products, only: [:index, :new, :show, :create]
   resources :users, only: [:show]
 
   get "RedBull", to:"users#edit2"
 end
+

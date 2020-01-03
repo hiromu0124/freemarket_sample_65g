797063bffebafb9b2b1b7aa2b48038288168b134
hiromu0124 <totohiro0124@icloud.com>
2020-01-03 16:12:25 +0900

    product_edit機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   
   devise_for :users
   root to: 'products#index'
-  resources :products, only: [:index, :new, :show]
+  resources :products, only: [:index, :new, :show, :create]
   resources :users, only: [:show]
-
+  resources :product_comments, only: [:new,:create]
   get "RedBull", to:"users#edit2"
+  post "create2", to:"products#create2"
 end

5710f0b7a489a365e79aa4b6f152983926fca88f
hiromu0124 <totohiro0124@icloud.com>
2020-01-06 13:58:29 +0900

    商品一覧表示機能追加

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,17 @@
 class ProductsController < ApplicationController
   def index
-    
+    @category_woman=ProductCategory.find(1)
+    @product_woman=Product.where(product_category_id:@category_woman.id)
+    @product_woman_image=ProductImage.where(product:@product_woman.ids)
+    @category_man=ProductCategory.find(2)
+    @product_man=Product.where(product_category_id:@category_man.id)
+    @product_man_image=ProductImage.where(product:@product_man.ids)
+    @category_toy=ProductCategory.find(3)
+    @product_toy=Product.where(product_category_id:@category_toy.id)
+    @product_toy_image=ProductImage.where(product:@product_toy.ids)
+    @category_camera=ProductCategory.find(5)
+    @product_camera=Product.where(product_category_id:@category_camera.id)
+    @product_camera_image=ProductImage.where(product:@product_camera.ids)
   end
 
   def new

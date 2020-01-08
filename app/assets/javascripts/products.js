010fcc465f90dcab684031fb6034d6c580909227
tsutomu3011 <t.08032323556@gmail.com>
2020-01-08 11:02:23 +0900

    ファイルプレビュー機能

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,12 +1,13 @@
 $(document).on('turbolinks:load', function() {
   // 画像用のinputを生成する関数
+  console.log(2)
   const buildFileField = (index)=> {
     const html = `<input class="js-file" type="file" name="product[product_images_attributes][0][image]" id="product_product_images_attributes_0_image"><span class="js-remove">削除</span>`;
     return html;
   }
 
   const buildImg = (index, url)=> {
-    const html = <img data-index="${index}" src="${url}" width="100px" height="100px">;
+    const html = `<img data-index="${index}" src="${url}" width="100px" height="100px">`;
     return html;
   }
 
@@ -19,10 +20,15 @@ $(document).on('turbolinks:load', function() {
     fileIndex.shift();
     // 末尾の数に1足した数を追加する
     fileIndex.push(fileIndex[fileIndex.length - 1] + 1)
-    
+
+    const targetIndex = $(this).parent().data('index');
+    // ファイルのブラウザ上でのURLを取得する
+    const file = e.target.files[0];
+    const blobUrl = window.URL.createObjectURL(file);
+    $('.previews').append(buildImg(targetIndex, blobUrl));
   });
 
-  $('#image-box').on('click', '.js-remove', function() {
+  $('.js-remove').on('click', '.js-remove', function() {
     $(this).parent().remove();
     // 画像入力欄が0個にならないようにしておく
     if ($('.js-file').length == 0) $('#image-box').append(buildFileField(fileIndex[0]));

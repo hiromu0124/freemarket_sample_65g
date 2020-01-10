ddcd2e3ff098fff445c53ac421d35745f583c81b
hiromu0124 <totohiro0124@icloud.com>
2020-01-10 14:10:32 +0900

    既存の値を表示する

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,6 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: [:show, :destroy, :edit]
+  
   def index
     @category_woman=ProductCategory.find(1)
     @product_womans=Product.where(product_category_id:@category_woman.id)
@@ -38,13 +40,12 @@ class ProductsController < ApplicationController
   end
 
   def show
-    @product = Product.find(params[:id])
     @category_product=@product.product_category_id
     @user_product= @product.user.id
     @product_category=Product.where(product_category_id:@category_product)
     @product_user=Product.where(user_id:@user_product)
-    @category_image=ProductImage.where(product_id:@product_category).limit(6)
-    @product_image=ProductImage.where(product_id: @product_user.ids).limit(6)
+    @category_image=ProductImage.where(product_id:@product_category)
+    @product_image=ProductImage.where(product_id: @product_user.ids)
     @product_0 = Product.find_by(id:@product.id-1)
     @product_1 = Product.find_by(id:@product.id+1)
     @images = ProductImage.where(product_id: params[:id]).limit(1)
@@ -52,15 +53,33 @@ class ProductsController < ApplicationController
     @product_comment=ProductComment.new
   end
 
+  def destroy
+    if @product.destroy 
+      redirect_to root_path
+    else
+      redirect_to product_destroy_miss_path
+    end
+  end
+
+  def destroy_miss
+  end
+
+
 
   def edit
     
   end
 
-  private
+
+private
   def comment_params
     params.require(:product_comment).permit(:comment).merge(user_id: current_user.id, product_id: @products.id)
   end
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+
 
   def product_params
     params.require(:product).permit(:name, :product_explain, :price, :product_category_id, :product_brand_id, :product_send_day, :prefecture_id, :product_condition, :product_fee, product_images_attributes: [:image]).merge(user_id: current_user.id, transaction_status: '出品中')

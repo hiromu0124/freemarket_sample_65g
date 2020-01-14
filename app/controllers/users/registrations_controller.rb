5184d1a6a5618a7c959cb9aed551d47f1c8e4802
nakashun1129 <nakashun1129@icloud.com>
2020-01-14 11:23:14 +0900

    レイアウト追加

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
+  layout 'newuser'
   before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
+
 
   def new  # 新規インスタンス作成
     @user = User.new

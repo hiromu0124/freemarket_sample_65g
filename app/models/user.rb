b216826fc73ed1f599c81ee5bd3d41de3156e103
nakashun1129 <nakashun1129@icloud.com>
2020-01-14 17:51:15 +0900

    SNS認証機能の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :omniauthable, omniauth_providers: [:facebook, :twitter, :google_oauth2]
+         :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
   
   def self.from_omniauth(auth)
   sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
